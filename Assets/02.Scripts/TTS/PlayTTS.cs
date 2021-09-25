7f78138a35e8a86b9ef69854af387272ed73fde4
SUHYE0N-KIM <84186059+SUHYE0N-KIM@users.noreply.github.com>
2021-09-26 01:55:02 +0900

    TTS STT ADD

---
+++ Assets/02.Scripts/TTS/PlayTTS.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayTTS : MonoBehaviour
+{
+
+    string LINK;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    public void click()
+    {
+        LINK = "いらっしゃいませ";
+        StartCoroutine(DownloadTheAudio());
+    }
+
+    public void click2()
+    {
+        LINK = "寿司盛り合わせください";
+        StartCoroutine(DownloadTheAudio());
+    }
+
+    public void click3()
+    {
+        LINK = "かしこまりました";
+        StartCoroutine(DownloadTheAudio());
+    }
+
+    public void click4()
+    {
+        LINK = "いただきます";
+        StartCoroutine(DownloadTheAudio());
+    }
+
+
+    IEnumerator DownloadTheAudio()
+    { //いらっしゃいませ
+        string url = "https://tts-translate.kakao.com/read?format=mpeg&lang=ja&txt=" + LINK;
+        //string url = "https://tts-translate.kakao.com/newtone?message="+LINK;
+        WWW www = new WWW(url);
+        while (!www.isDone)
+        {
+            yield return 0;
+        }
+        GetComponent<AudioSource>().clip = NAudioPlayer.FromMp3Data(www.bytes);
+        audioSource.Play();
+    }
+}
