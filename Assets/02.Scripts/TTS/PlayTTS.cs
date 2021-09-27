6aa8eb91ce43a1792714761047a2145cf7404a11
SUHYE0N-KIM <84186059+SUHYE0N-KIM@users.noreply.github.com>
2021-09-28 01:06:07 +0900

    Eat Interaction ADD

--- Assets/02.Scripts/TTS/PlayTTS.cs
+++ Assets/02.Scripts/TTS/PlayTTS.cs
@@ -16,32 +16,46 @@ public class PlayTTS : MonoBehaviour
     public void click()
     {
         LINK = "いらっしゃいませ";
-        StartCoroutine(DownloadTheAudio());
+        StartCoroutine(kakao());
     }
 
     public void click2()
     {
-        LINK = "寿司盛り合わせください";
-        StartCoroutine(DownloadTheAudio());
+        //LINK = "寿司盛り合わせください";
+        LINK = "c_lt_shinji_2.1.10_0-nvoice_shinji_2.1.10_d2ad418645a667f9d265f3e3ac7a6d16-1632754314200";
+        StartCoroutine(papago());
     }
 
     public void click3()
     {
         LINK = "かしこまりました";
-        StartCoroutine(DownloadTheAudio());
+        StartCoroutine(kakao());
     }
 
     public void click4()
     {
-        LINK = "いただきます";
-        StartCoroutine(DownloadTheAudio());
+        //LINK = "いただきます";
+        LINK = "c_lt_shinji_2.1.10_1-nvoice_shinji_2.1.10_6b1bf127bf30adcdde99bb1ccf8e6eb9-1632754679338";
+        StartCoroutine(papago());
     }
 
 
-    IEnumerator DownloadTheAudio()
-    { //いらっしゃいませ
+    IEnumerator kakao()
+    { 
         string url = "https://tts-translate.kakao.com/read?format=mpeg&lang=ja&txt=" + LINK;
         //string url = "https://tts-translate.kakao.com/newtone?message="+LINK;
+        WWW www = new WWW(url);
+        while (!www.isDone)
+        {
+            yield return 0;
+        }
+        GetComponent<AudioSource>().clip = NAudioPlayer.FromMp3Data(www.bytes);
+        audioSource.Play();
+    }
+
+    IEnumerator papago()
+    { 
+        string url = "https://papago.naver.com/apis/tts/" + LINK;
         WWW www = new WWW(url);
         while (!www.isDone)
         {
---
+++ Assets/eating.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eating : MonoBehaviour
+{
+
+    public GameObject sushi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.position.z);
+        if (transform.position.z > -4.2) 
+        {
+            Destroy(sushi);
+        }
+    }
+}
