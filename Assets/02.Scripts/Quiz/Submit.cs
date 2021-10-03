9714a8198b839a2dbd598de189c62f527164103f
SUHYE0N-KIM <84186059+SUHYE0N-KIM@users.noreply.github.com>
2021-10-04 03:30:38 +0900

    quiz ADD

---
+++ Assets/02.Scripts/Quiz/Submit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Submit : MonoBehaviour
+{
+    public Text answer;
+
+    public GameObject O;
+    public GameObject X;
+
+    int timer = 3;
+    
+    public void submit()
+    {
+        if (answer.text == "すみません")
+        {
+            X.SetActive(false);
+            O.SetActive(true);
+
+            StartCoroutine(NextScene());
+        }
+
+        else
+        {
+            O.SetActive(false);
+            X.SetActive(true);
+        }
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(timer);
+        SceneManager.LoadScene("Menu");
+    }
+}
